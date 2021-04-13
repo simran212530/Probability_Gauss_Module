13f5a673873bddb23dbef74aac2a56ed524a201d
Simran <62848346+simran212530@users.noreply.github.com>
2021-04-13 19:17:11 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob_gauss_binomial',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['prob_gauss_binomial'],
+      author = 'Gursimran Kaur',
+      author_email = 'gursimran18336@iiitd.ac.in',
+      zip_safe=False)
